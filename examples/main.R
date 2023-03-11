37746d44279f7be865627e1494c0b88ca7eeb325
Howard Thom <howard.thom@bristol.ac.uk>
2023-03-11 18:07:11 +0000

    Added an example to test the package

--- examples/main.R
+++ examples/main.R
@@ -10,19 +10,8 @@
 #library(devtools) # Make sure that the devtools library is loaded
 #install_github("Bogdasayen/reeevr_1");
 
-example_workbook_name <- "test_workbook_2.xlsx"
-example_output_file <- "test_output.R"
+example_workbook_name <- "examples/test_workbook_2.xlsx"
+example_output_file <- "examples/test_output.R"
 
 
-reeevr(workbook_name = "test_workbook_2.xlsx", "test_output.R")
-
-
-# Form a simple summary of named cells
-named_cells <- xlsx_names("test_workbook_2.xlsx")
-n_named_cells <- dim(named_cells)[1]
-named_cells_summary <- matrix(nrow = n_named_cells, ncol = 2)
-colnames(named_cells_summary) <- c("name", "location")
-for(i_named_cell in 1:n_named_cells) {
-  # Probably not good to coerce into matrix of strings
-  named_cells_summary[i_named_cell, ] <- as.matrix(named_cells[i_named_cell, c("name", "formula")])
-}
+reeevr(example_workbook_name, example_output_file)
--- examples/test_output.R
+++ examples/test_output.R
@@ -10,5 +10,6 @@ cost_bleed <- 400
 qalys_stroke <- 14
 qalys_bleed <- 12
 qalys_well <- 20
+
 # Calculate the ICER 
 icer <- ((cost_course_warfarin+probability_bleed_warfarin*cost_bleed+probability_stroke_warfarin*cost_stroke)-(cost_course_aspirin+probability_bleed_aspirin*cost_bleed+probability_stroke_aspirin*cost_stroke))/((probability_bleed_warfarin*qalys_bleed+probability_stroke_warfarin*qalys_stroke+(1-probability_bleed_warfarin-probability_stroke_warfarin)*qalys_well)-(probability_bleed_aspirin*qalys_bleed+probability_stroke_aspirin*qalys_stroke+(1-probability_bleed_aspirin-probability_stroke_aspirin)*qalys_well))
